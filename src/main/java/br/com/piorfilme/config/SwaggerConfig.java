e620086310ee02a6e1a76bf58bcbaf1c8143b90f
Roberson Carlos <robersonfox@gmail.com>
2021-04-19 10:56:08 -0300

    Swagger e libs necessárias para o projecto

---
+++ src/main/java/br/com/piorfilme/config/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package br.com.piorfilme.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.piorfilme"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, responseMessage())
+                .globalResponseMessage(RequestMethod.POST, responseMessage())
+                .apiInfo(apiInfo());
+
+    }
+
+    private List<ResponseMessage> responseMessage() {
+        return new ArrayList<ResponseMessage>() {
+            {
+                add(new ResponseMessageBuilder().code(200).message("Aceito").build());
+                add(new ResponseMessageBuilder().code(400).message("Bad Request").build());
+                add(new ResponseMessageBuilder().code(201).message("Created").build());
+            }
+        };
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Farm Farm")
+                .description("Uma API RESTful para possibilitar a leitura da lista de indicados e vencedores da categoria Pior Filme do Golden Raspberry Awards.")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                .contact(new Contact("robersonfox", "", "robersonfox@gmail.com"))
+                .build();
+    }
+}
