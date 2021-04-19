bdd3bb3cfa5d630bcf89bd7f21cad1eb9a45b271
Roberson Carlos <robersonfox@gmail.com>
2021-04-19 10:51:53 -0300

    Initialize

---
+++ src/main/java/br/com/piorfilme/PiorFilmeApplication.java
@@ -0,0 +1,13 @@
+package br.com.piorfilme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiorFilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiorFilmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/piorfilme/PiorFilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.piorfilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiorFilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
