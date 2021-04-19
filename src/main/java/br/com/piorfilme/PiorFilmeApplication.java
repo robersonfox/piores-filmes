ef45b0d0c5cbc902d199743c6b037562bcd552bd
Roberson Carlos <robersonfox@gmail.com>
2021-04-19 11:00:40 -0300

    Configurar application.yaml

--- src/main/java/br/com/piorfilme/PiorFilmeApplication.java
+++ src/main/java/br/com/piorfilme/PiorFilmeApplication.java
@@ -1,13 +1,23 @@
 package br.com.piorfilme;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PiorFilmeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PiorFilmeApplication.class, args);
 	}
 
+	@PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 }
